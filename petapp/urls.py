9acfe65f56015beb858b8d1e614ae7b4656b9a26
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-11-01 00:49:11 +0600

    setup pages in django

--- petapp/urls.py
+++ petapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('login/', views.login, name='login'),
   path('registration/', views.registration, name='registration'),
+  path('about/', views.about, name='about'),
+  path('items/', views.items, name='items'),
+  path('item/', views.item, name='item'),
+  path('createPet/', views.createPet, name='createPet'),
 ]
