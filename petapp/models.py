a51736358713f974714fcb78e22ff1f7c646109a
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-11-07 22:05:33 +0600

    pet post create functionality

--- petapp/models.py
+++ petapp/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
 
 class UserProfile(AbstractUser):
   email = models.EmailField(unique=True)
@@ -17,3 +20,17 @@ class UserProfile(AbstractUser):
 
   def __str__(self):
       return self.username
+
+class PetPost(models.Model):
+  user = models.ForeignKey('UserProfile', on_delete=models.CASCADE, related_name='pet_posts')
+  title = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='uploads/', blank=False, null=False)
+  pet_type = models.CharField(max_length=50, blank=False, null=False)
+  gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female')])
+  description = models.TextField(blank=False, null=False)
+  location = models.CharField(max_length=100, blank=False, null=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return self.title
