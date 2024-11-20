15a10e5307af33f3ecd6f5c6082087fef286d418
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-11-20 20:30:23 +0600

    feat: converted homepage design

--- petproject/settings.py
+++ petproject/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,6 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# Load environment variables from the .env file
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
