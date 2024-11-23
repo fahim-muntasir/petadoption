937630aae5d5db2872238bd13066a9f655e366a6
Fahim muntasir <fahimmuntasir0909@gmail.com>
2024-11-23 19:07:49 +0600

    fixed donet button logic and 404 page

--- petproject/settings.py
+++ petproject/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOGIN_URL = '/login/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
